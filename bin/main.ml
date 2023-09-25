172bf94bc2f98cc157ca31fddd171c00b57b7fb6
ajlow2000 <ajlow2000@gmail.com>
2023-09-24 23:51:01 -0600

    WIP: Move helper code to lib module

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello, World!"
+open Lib
+
+let dir = "./foo"
+
+
+let () = match Sys.file_exists dir with
+    | false -> Printf.printf "Specified directory (\"%s\") does not exist\n" dir
+    | true -> match FsHelpers.dir_is_empty dir with
+        | true -> print_endline "Directory is empty"
+        | false -> List.iter print_endline (FsHelpers.dir_contents dir)
+
+
+
+
+
+
