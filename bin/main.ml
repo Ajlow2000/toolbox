d34f7896121dded08fda1c7cc825ea7cb125359e
ajlow2000 <ajlow2000@gmail.com>
2023-09-25 00:52:43 -0600

    WIP(dirty_git): Progress Save for building list of git directories

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,12 @@
-open Lib
+open Lib.FsHelpers
 
 let dir = "./foo"
 
-
 let () = match Sys.file_exists dir with
     | false -> Printf.printf "Specified directory (\"%s\") does not exist\n" dir
-    | true -> match FsHelpers.dir_is_empty dir with
+    | true -> match dir_is_empty dir with
         | true -> print_endline "Directory is empty"
-        | false -> List.iter print_endline (FsHelpers.dir_contents dir)
+        | false -> List.iter print_endline (dir_contents dir)
 
 
 
--- lib/fsHelpers.ml
+++ lib/fsHelpers.ml
@@ -2,21 +2,37 @@
  * "." and ".."
  *)
 let dir_is_empty dir =
-  Array.length (Sys.readdir dir) = 0
+    Array.length (Sys.readdir dir) = 0
 
 (** [dir_contents] returns the paths of all regular files that are
  * contained in [dir]. Each file is a path starting with [dir].
   *)
 let dir_contents dir =
-  let rec loop result = function
-    | f::fs when Sys.is_directory f ->
-          Sys.readdir f
-          |> Array.to_list
-          |> List.map (Filename.concat f)
-          |> List.append fs
-          |> loop result
-    | f::fs -> loop (f::result) fs
-    | []    -> result
-  in
+    let rec loop result = function
+        | f::fs when Sys.is_directory f ->
+            Sys.readdir f
+            |> Array.to_list
+            |> List.map (Filename.concat f)
+            |> List.append fs
+            |> loop result
+        | f::fs -> loop (f::result) fs
+        | []    -> result
+    in
     loop [] [dir]
 
+(** [dirty_git_repos] returns the path of all directories that are git repos
+ * contained within the specified [dir]. Each directory is a path 
+ * starting with [dir]
+ *)
+let dirty_git_repos dir =
+    let rec loop result = function
+        | f::fs when Sys.is_directory f ->
+            Sys.readdir f
+            |> Array.to_list
+            |> List.map (Filename.concat f)
+            |> List.append fs
+            |> loop result
+        | f::fs -> loop (f::result) fs
+        | []    -> result
+    in
+    loop [] [dir]
