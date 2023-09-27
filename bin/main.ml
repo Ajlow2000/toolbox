e244dce21871868160f18eddebe65d9dff470c46
ajlow2000 <ajlow2000@gmail.com>
2023-09-27 10:59:58 -0600

    feat: Add basic placeholder cli

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,11 @@
 open Lib.FsHelpers
 
-let dir = "./foo"
+let target_dir = match Sys.getenv_opt "HOME" with
+    | Some v -> v
+    | None -> "./"
 
-let () = match Sys.file_exists dir with
-    | false -> Printf.printf "Specified directory (\"%s\") does not exist\n" dir
-    | true -> match dir_is_empty dir with
-        | true -> print_endline "Directory is empty"
-        | false -> List.iter print_endline (git_dirs dir)
+let git_repos = match Sys.file_exists target_dir with
+    | false -> []
+    | true -> git_dirs target_dir
 
-
-
-
-
-
+let () = List.iter print_endline (git_repos)
