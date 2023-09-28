ab9930b61428466cadf3c064a97d9baf1695bb28
ajlow2000 <ajlow2000@gmail.com>
2023-09-27 19:30:03 -0600

    refactor(main): Add double ;; to end multilie expressions

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,11 @@ open Lib.FsHelpers
 let target_dir = match Sys.getenv_opt "HOME" with
     | Some v -> v
     | None -> "./"
+;;
 
 let git_repos = match Sys.file_exists target_dir with
     | false -> []
     | true -> git_dirs target_dir
+;;
 
 let () = List.iter print_endline (git_repos)
