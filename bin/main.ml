c352db24eb535c87b53e6752aa02c137bbad83fd
ajlow2000 <ajlow2000@gmail.com>
2023-09-28 09:39:01 -0600

    refactor(fmt): Add ocamlformat and reformat project

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,15 @@
 open Lib.FsHelpers
 
-let target_dir = match Sys.getenv_opt "HOME" with
-    | Some v -> v
-    | None -> "./"
+let target_dir =
+  match Sys.getenv_opt "HOME" with
+  | Some v -> v
+  | None -> "./"
 ;;
 
-let git_repos = match Sys.file_exists target_dir with
-    | false -> []
-    | true -> git_dirs target_dir
+let git_repos =
+  match Sys.file_exists target_dir with
+  | true -> git_dirs target_dir
+  | false -> []
 ;;
 
-let () = List.iter print_endline (git_repos)
+let () = List.iter print_endline git_repos
--- lib/fsHelpers.ml
+++ lib/fsHelpers.ml
@@ -1,17 +1,18 @@
 (** [git_dir] recursively searches through a specificied
     [dir] and returns a the paths to all directories
-    that contain a .git/ subdir.
- *
-*)
+    that contain a .git/ subdir. *)
 let git_dirs dir =
-    let rec add_contents accu filename =
-        match Sys.file_exists filename with
-        | false -> accu
-        | true -> 
-            match Filename.basename filename with
-            | ".git" -> Filename.dirname filename :: accu
-            | _ when Sys.is_directory filename -> Sys.readdir filename
-                |> Array.map (Filename.concat filename)
-                |> Array.fold_left add_contents accu
-            | _ -> accu
-    in add_contents [] dir
+  let rec add_contents accu filename =
+    match Sys.file_exists filename with
+    | true ->
+      (match Filename.basename filename with
+       | ".git" -> Filename.dirname filename :: accu
+       | _ when Sys.is_directory filename ->
+         Sys.readdir filename
+         |> Array.map (Filename.concat filename)
+         |> Array.fold_left add_contents accu
+       | _ -> accu)
+    | false -> accu
+  in
+  add_contents [] dir
+;;
