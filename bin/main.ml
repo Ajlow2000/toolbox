ce2c9ed829e39a9a7674ef29408e99b83ee97c5c
ajlow2000 <ajlow2000@gmail.com>
2023-11-13 18:20:53 -0700

    chore: Progress save before startng over

--- bin/main.ml
+++ bin/main.ml
@@ -17,17 +17,9 @@ let ignore =
 let repos =
   printf "Target Dir: %s\n-------------------\n" target_dir;
   match file_exists target_dir with
-  | `Yes -> git_dirs target_dir ~ignore:ignore
+  | `Yes -> git_dirs target_dir ~ignore
   | _ -> []
 ;;
 
-let print_entry path = 
-    let width = Core_unix.system "tput cols" in
-    let rec string n s =
-        if n = 0 then "" else s ^ string (n - 1) s
-    in
-    printf "%s\n" (string (width / 2) "<>");
-    printf "%s\n" path;
-    printf "%s\n\n" "- - - - - - - - - - - - - - - - - - - -"
-
+let print_entry path = printf "%s\n" path
 let () = List.iter repos ~f:print_entry
