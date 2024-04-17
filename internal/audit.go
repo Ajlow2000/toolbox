5bb792a0ff76d9dacdd2003ded273e5300ee5f00
ajlow2000 <ajlow2000@gmail.com>
2024-04-17 21:47:45 +0000

    WIP(working on audit functionality):

--- internal/audit.go
+++ internal/audit.go
@@ -1,12 +1,42 @@
 package internal
 
-func getStatus() string {
-    return "TODO"
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type gitInfo struct {
+    url string
+    isOutOfSync bool
+    dirtyFiles []string
 }
 
-func AuditDir(dir string, recursive bool) int {
-    if (recursive) {
-        println("UNDER DEVELOPMENT" + dir)
+
+func getStatus(dir string) {
+    if (path.IsDir()) {
+    } else {
+        // log error, files cant be git repos
     }
-    return 0
+}
+
+func findGitRepos(targetDir string, repos *[]gitInfo) {
+    files, err := os.ReadDir(targetDir)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        if (file.IsDir() && file.Name() == ".git" ) {
+            findGitRepos(targetDir, repos)
+        }
+    }
+}
+
+func AuditDir(targetDir string) {
+    localRepos := make([]gitInfo, 20)
+    findGitRepos(targetDir, &localRepos)
+    return
 }
