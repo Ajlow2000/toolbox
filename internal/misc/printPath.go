7143a3c588ad62c6bc6b210f35d3ff4d10519e12
ajlow2000 <ajlow2000@gmail.com>
2024-05-07 16:53:33 -0600

    feat: migrate tool impl to internal

--- internal/misc/printPath.go
+++ internal/misc/printPath.go
@@ -1,5 +1,14 @@
 package misc
 
-func main()  {
-    println("Print Path INTERNAL under development")
+import (
+	"os"
+	"strings"
+)
+
+// PrintPath prints entries in $PATH on newlines
+func PrintPath()  {
+    var path = strings.Split(os.Getenv("PATH"), ":")
+    for _, entry := range path {
+        println(entry)
+    }
 }
