57e73b87636bbc490440d57166ceb3fbe579b793
ajlow2000 <ajlow2000@gmail.com>
2024-05-09 20:05:23 +0000

    WIP(auditDir)!: Blocked audit dir functionality to push framework to main
    BREAKING CHANGE:

--- internal/git/auditDir.go
+++ internal/git/auditDir.go
@@ -3,15 +3,11 @@ package git
 import (
 	"errors"
 	"fmt"
-	"io"
-	"log/slog"
 	"os"
-	"os/user"
 	"path/filepath"
 	"slices"
 	"time"
-
-	"github.com/go-git/go-git/v5"
+	// "github.com/go-git/go-git/v5"
 )
 
 
@@ -127,100 +123,104 @@ func collectGitInfo(path string) (gitInfo, error)  {
         }
         return info, nil
     } else {
-        return errors.New("Error collecting git info.  Provided path is not a directory.")
+        return gitInfo{}, errors.New("Error collecting git info.  Provided path is not a directory.")
     }
 }
 
 // AuditDir takes a string [target] and searches for git repos within the specified path.
 // Generates and outputs a report of the status of found git repositories.
 func AuditDir(target string, logPath string, ignore []string)  {
-    // Initialize Logger
-    if logPath == "" {
-        slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
-    } else if logPath == "stderr" {
-        slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, nil)))
-    } else {
-        logPath, err := validateLogPath(logPath)
-        if err != nil {
-            fmt.Println(err.Error())
-            return
-        }
-        logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-        if err != nil {
-            fmt.Println("An error occured when opening '" + logPath + "' in append mode")
-            fmt.Println("\t" + err.Error())
-            return
-        }
-        defer logFile.Close()
-        fmt.Println("Initialized logging to: " + logPath)
-        slog.SetDefault(slog.New(slog.NewJSONHandler(logFile, nil)))
-    }
-
-    // Set globals
-    IGNORE = ignore
-
-    // Validate target
-    if target == "" {
-        var err error
-        target, err = os.UserHomeDir()
-        if err != nil {
-            slog.Error(err.Error())
-        }
-    } else {
-        err := validateTarget(target)
-        if err != nil {
-            slog.Error(err.Error())
-            return
-        }
-    }
-
-    // Begin Audit
-    currentUser, err := user.Current()
-    if err != nil {
-        slog.Error(err.Error())
-    }
-    username := currentUser.Username
-    slog.Info("Beginning an audit of git repositories", slog.Any("target", target), slog.Any("user", username), slog.Any("audit_status", "initialized"))
-    fmt.Println("Beginning an audit of git repositories in: " + target)
-
-    path, err := filepath.Abs(target)
-    if err != nil {
-        slog.Error(err.Error())
-        return
-    }
-    if !slices.Contains(IGNORE, target) {
-        entries, err := os.ReadDir(target)
-        if err != nil {
-            slog.Error(err.Error())
-            return
-        }
-        for _, entry := range entries {
-            if entry.IsDir() {
-                dirPath := filepath.Join(path, entry.Name())
-                isGitRepo, err := isGitRepo(dirPath)
-                if err != nil {
-                    slog.Error(err.Error())
-                    return
-                }
-                if isGitRepo {
-                    GIT_REPOS = append(GIT_REPOS, dirPath)
-                } else {
-                    // recurse
-                    if !slices.Contains(IGNORE, dirPath) {
-                        recurseInSearchOfGit(dirPath)
-                    }
-                }
-            }
-        }
-    }
-
-    // fmt.Println("Found the following repos:")
-    // for _, element := range GIT_REPOS {
-    //     fmt.Println(element)
+    fmt.Println("Audit Dir is under development")
+    return
+    //
+    //
+    // // Initialize Logger
+    // if logPath == "" {
+    //     slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+    // } else if logPath == "stderr" {
+    //     slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, nil)))
+    // } else {
+    //     logPath, err := validateLogPath(logPath)
+    //     if err != nil {
+    //         fmt.Println(err.Error())
+    //         return
+    //     }
+    //     logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    //     if err != nil {
+    //         fmt.Println("An error occured when opening '" + logPath + "' in append mode")
+    //         fmt.Println("\t" + err.Error())
+    //         return
+    //     }
+    //     defer logFile.Close()
+    //     fmt.Println("Initialized logging to: " + logPath)
+    //     slog.SetDefault(slog.New(slog.NewJSONHandler(logFile, nil)))
     // }
-
-    
-    fmt.Println("Succesfully completed")
-    slog.Info("Concluded audit", slog.Any("target", target), slog.Any("user", username), slog.Any("audit_status", "completed"))
+    //
+    // // Set globals
+    // IGNORE = ignore
+    //
+    // // Validate target
+    // if target == "" {
+    //     var err error
+    //     target, err = os.UserHomeDir()
+    //     if err != nil {
+    //         slog.Error(err.Error())
+    //     }
+    // } else {
+    //     err := validateTarget(target)
+    //     if err != nil {
+    //         slog.Error(err.Error())
+    //         return
+    //     }
+    // }
+    //
+    // // Begin Audit
+    // currentUser, err := user.Current()
+    // if err != nil {
+    //     slog.Error(err.Error())
+    // }
+    // username := currentUser.Username
+    // slog.Info("Beginning an audit of git repositories", slog.Any("target", target), slog.Any("user", username), slog.Any("audit_status", "initialized"))
+    // fmt.Println("Beginning an audit of git repositories in: " + target)
+    //
+    // path, err := filepath.Abs(target)
+    // if err != nil {
+    //     slog.Error(err.Error())
+    //     return
+    // }
+    // if !slices.Contains(IGNORE, target) {
+    //     entries, err := os.ReadDir(target)
+    //     if err != nil {
+    //         slog.Error(err.Error())
+    //         return
+    //     }
+    //     for _, entry := range entries {
+    //         if entry.IsDir() {
+    //             dirPath := filepath.Join(path, entry.Name())
+    //             isGitRepo, err := isGitRepo(dirPath)
+    //             if err != nil {
+    //                 slog.Error(err.Error())
+    //                 return
+    //             }
+    //             if isGitRepo {
+    //                 GIT_REPOS = append(GIT_REPOS, dirPath)
+    //             } else {
+    //                 // recurse
+    //                 if !slices.Contains(IGNORE, dirPath) {
+    //                     recurseInSearchOfGit(dirPath)
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // // fmt.Println("Found the following repos:")
+    // // for _, element := range GIT_REPOS {
+    // //     fmt.Println(element)
+    // // }
+    //
+    // 
+    // fmt.Println("Succesfully completed")
+    // slog.Info("Concluded audit", slog.Any("target", target), slog.Any("user", username), slog.Any("audit_status", "completed"))
 }
 
