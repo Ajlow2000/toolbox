907d2d6e6bff9ef015d740e6fb8a88a3ceb0deb4
ajlow2000 <ajlow2000@gmail.com>
2024-07-01 20:32:32 +0000

    feat(addRepo): Build local dirname

--- cmd/git/addRepo.go
+++ cmd/git/addRepo.go
@@ -5,25 +5,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addRepoCmd represents the addRepo command
+var (
+    url = "";
+    path = "";
+)
+
 var addRepoCmd = &cobra.Command{
 	Use:   "add-repo",
 	Short: "Utility for cloning repo's with standard name",
 	Long: "",
 	Run: func(cmd *cobra.Command, args []string) {
-		git.AddRepo()
+		git.AddRepo(url, path)
 	},
 }
 
 func init() {
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addRepoCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addRepoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+    addRepoCmd.Flags().StringVar(&url, "url", "", "The url pointing at a git repository")
+    addRepoCmd.Flags().StringVar(&path, "path", "", "The path to clone the specified url into")
 }
--- internal/git/addRepo.go
+++ internal/git/addRepo.go
@@ -3,25 +3,47 @@ package git
 import (
 	"fmt"
 	"os"
+	// "os"
 	"strings"
-
-	"github.com/go-git/go-git/v5"
+	// "github.com/go-git/go-git/v5"
 )
 
-func AddRepo() {
-	gitUrl := "https://github.com/Ajlow2000/home-manager"
-	repoHome := "foo"
-	params := strings.Split(gitUrl, "/")
-	
-	userName := params[3]
-	projectName := params[4]
-	localName := userName + "_" + projectName
-	fmt.Println(localName)
-	cloneOptions := git.CloneOptions{
-		URL: gitUrl,
-		Progress: os.Stdout,
-	}
-	git.PlainClone(repoHome, false, &cloneOptions)
+// Build a name for the local checkout of the specified url.
+// Namespaces repos under the owner's username (in lower case)
+func buildLocalDirName(url string) string {
+    userName := ""
+    projectName := ""
+
+    if strings.HasPrefix(url, "git@") {     // ssh
+        url = strings.Split(url, ":")[1]
+        params := strings.Split(url, "/")
+        userName = params[0]
+        projectName = params[1]
+    } else {                                // https
+        params := strings.Split(url, "/")
+        userName = params[3]
+        projectName = params[4]
+    }
+
+    userName = strings.ToLower(userName)
+    projectName, _ = strings.CutSuffix(projectName, ".git")
+
+    return userName + "_" + projectName;
+}
+
+func AddRepo(url string, path string) {
+	localName := buildLocalDirName(url)
+    fmt.Fprintln(os.Stderr, localName)
+
+	// cloneOptions := git.CloneOptions{
+	// 	URL: url,
+	// 	Progress: os.Stdout,
+	// }
+
+    // repo, err := git.PlainClone(path, false, &cloneOptions)
+    // if err != nil {
+    //     fmt.Fprintln(os.Stderr, "An error occured when cloning '" + url + "' into '" + path + "'")
+    // }
 	// git -C repoHome clone gitUrl localName
 	// git -C "$HOME/repos" clone $GIT_URL $PROJECT_DIR
 }
