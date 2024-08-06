292138ba14424b3d183a7882ea100de4d1c9d282
ajlow2000 <ajlow2000@gmail.com>
2024-08-06 18:12:35 +0000

    feat: Add printpath under new flake format

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        printPath = {
+            url = "github:Ajlow2000/print-path";
+            flake = true;
+        };
     };
 
-    outputs = { self, nixpkgs }:
+    outputs = { self, nixpkgs, printPath }:
     let
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-        # packages.${system}.default = 
+        packages.${system}.print-path =
+            printPath.packages.${system}.default;
 
         devShell.x86_64-linux =
             pkgs.mkShell {
